c70e7157460da312c4d338ac4b77259c7b093247
Siraj Mohamed <serag9923@gmail.com>
2025-07-06 18:13:23 +0300

    try 3

--- src/app/quran/[surahId]/page.tsx
+++ src/app/quran/[surahId]/page.tsx
@@ -178,7 +178,7 @@ export default function SurahPage() {
             }}
             className='cursor-pointer mb-6 px-4 py-2 bg-green-200 text-green-800 rounded-lg hover:bg-green-300 transition flex justify-center items-center'
           >
-            <span className=''>→</span> السورة السابقة
+            <span className=''>→</span><span> السورة السابقة</span>
           </button>
           <button
             onClick={() => {
