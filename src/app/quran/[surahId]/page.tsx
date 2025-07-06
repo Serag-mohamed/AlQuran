5b7ff4758fc4f43adb0aca099ae4fc1dd9ef7db3
Siraj Mohamed <serag9923@gmail.com>
2025-07-06 18:10:47 +0300

    try2

--- src/app/quran/[surahId]/page.tsx
+++ src/app/quran/[surahId]/page.tsx
@@ -176,7 +176,7 @@ export default function SurahPage() {
                 router.replace(`/quran/114`);
               else router.replace(`/quran/${Number(surahId) - 1}`);
             }}
-            className='cursor-pointer mb-6 px-4 py-2 bg-green-200 text-green-800 rounded-lg hover:bg-green-300 transition'
+            className='cursor-pointer mb-6 px-4 py-2 bg-green-200 text-green-800 rounded-lg hover:bg-green-300 transition flex justify-center items-center'
           >
             <span className=''>→</span> السورة السابقة
           </button>
@@ -185,9 +185,9 @@ export default function SurahPage() {
               if (surahName === 'سُورَةُ النَّاسِ') router.replace(`/quran/1`);
               else router.replace(`/quran/${Number(surahId) + 1}`);
             }}
-            className='cursor-pointer mb-6 px-4 py-2 bg-green-200 text-green-800 rounded-lg hover:bg-green-300 transition'
+            className='cursor-pointer mb-6 px-4 py-2 bg-green-200 text-green-800 rounded-lg hover:bg-green-300 transition flex justify-center items-center'
           >
-            السورة التالية <span className=''> ←</span>
+            <span>السورة التالية </span><span className=''> ←</span>
           </button>
         </div>
       </div>
